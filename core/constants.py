ed87695b56688e97f7bf81b9fa45ec9f6e762c97
Villa-dev <franciscoakajavi98@gmail.com>
2026-07-01 23:54:27 -0400

    refactor(core): Extraer enumeradores y cadenas estaticas a modulo de constantes para tipado seguro

---
+++ core/constants.py
@@ -0,0 +1,11 @@
+# Strings y enumeradores para evitar Hardcoding
+class EntityState:
+    IDLE = "IDLE"
+    WALK = "WALK"
+    ATTACK = "ATTACK"
+    DEAD = "DEAD"
+
+class ItemCategory:
+    WEAPON = "Weapons"
+    APPAREL = "Apparel"
+    MATERIAL = "Materials"
