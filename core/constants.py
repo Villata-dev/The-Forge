809acac261e1d7271c1b5de7913d6d5edf7d8a99
Villa-dev <franciscoakajavi98@gmail.com>
2026-07-04 22:45:00 -0400

    refactor(core): Extender enumeradores globales para integrar pociones e ingredientes botanicos

--- core/constants.py
+++ core/constants.py
@@ -1,11 +1,14 @@
-# Strings y enumeradores para evitar Hardcoding
+# Strings y enumeradores ampliados
 class EntityState:
     IDLE = "IDLE"
     WALK = "WALK"
     ATTACK = "ATTACK"
     DEAD = "DEAD"
+    SLEEP = "SLEEP"
 
 class ItemCategory:
     WEAPON = "Weapons"
     APPAREL = "Apparel"
     MATERIAL = "Materials"
+    INGREDIENT = "Ingredients"
+    POTION = "Potions"
