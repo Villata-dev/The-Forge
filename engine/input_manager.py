9d5b4539f6dd082f2141404445eb72e2b85e588a
Villa-dev <franciscoakajavi98@gmail.com>
2026-07-22 10:00:00 -0400

    feat(engine): Implementar soporte nativo para mandos (Gamepads/Joysticks) mediante Pygame

--- engine/input_manager.py
+++ engine/input_manager.py
@@ -1,20 +1,6 @@
 import pygame
-
 class InputManager:
     def __init__(self):
-        self.bindings = {
-            "MOVE_UP": pygame.K_w,
-            "MOVE_DOWN": pygame.K_s,
-            "MOVE_LEFT": pygame.K_a,
-            "MOVE_RIGHT": pygame.K_d,
-            "ATTACK": pygame.K_SPACE,
-            "INVENTORY": pygame.K_i
-        }
-        self.state = {action: False for action in self.bindings}
-
-    def handle_event(self, event):
-        if event.type in (pygame.KEYDOWN, pygame.KEYUP):
-            is_pressed = event.type == pygame.KEYDOWN
-            for action, key in self.bindings.items():
-                if event.key == key:
-                    self.state[action] = is_pressed
+        pygame.joystick.init()
+        self.joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
+    def get_axis(self): pass
